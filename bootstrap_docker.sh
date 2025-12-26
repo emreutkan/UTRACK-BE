3264ee925277761c2533636bfc43519917b74f1e
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-12-27 02:40:52 +0300

    Fix: Remove docker-compose-v2 to avoid package conflict with docker-compose-plugin

--- bootstrap_docker.sh
+++ bootstrap_docker.sh
@@ -27,7 +27,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-v2
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # 4. Manage Permissions
 # Allow 'ubuntu' user to run docker without sudo
